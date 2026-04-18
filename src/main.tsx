28b1d926eedb9f195aba1be45808962febc17a83
Jason Daniel <jasondaniel18@gmail.com>
2026-04-18 22:32:04 +0530

    minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { AppRouter } from './router'
 
+if (location.pathname.endsWith('/index.html')) {
+  history.replaceState(null, '', location.pathname.replace(/\/index\.html$/, '/') + location.search + location.hash)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
